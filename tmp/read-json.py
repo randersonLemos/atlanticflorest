7cecf7ab65f313cd2ad1e8cd0d71e322d5c234e1
Randerson Lemos <randerson.lemos@gmail.com>
2024-04-25 10:39:02 -0300

    Parsed json command

--- tmp/read-json.py
+++ tmp/read-json.py
@@ -4,9 +4,13 @@ if __name__ == '__main__':
     with open('canopy-height-resolution.json') as file:
         content = file.read()
 
-
+    lines = []
     for line in content.replace('\\r', '').split('\\n'):
         print(line)
+        lines.append(line)
+
+    with open('eecode.js', 'w') as file:
+        file.write( '\n'.join(lines) )
 
 
 
